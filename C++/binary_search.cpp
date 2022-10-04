ad20fe22a963da7e8f8ef4d345874fa847c4ed73
shreyapurohit <shreyapurohit94@gmail.com>
2022-10-04 18:18:27 +0530

    Update binary_search.cpp
    Made code changes to make the solution more efficient by using iterative approach

--- C++/binary_search.cpp
+++ C++/binary_search.cpp
@@ -1,40 +1,43 @@
-#include<iostream>
+#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int bsearch(int a[],int n,int s)
+
+int binarySearch(vector<int> v, int To_Find)
 {
-    int l=0,u=n-1;
-    int mid,flag=0;
-    while(l<=u)
-    {
-        mid=(l+u)/2;
-        if(s==a[mid])
-        {
-            flag=1;
-            break;
-        }
-        else
-        {
-            if(s>a[mid])
-                l=mid+1;
-            else if(s<a[mid])
-                u=mid-1;
-        }
-    }
-    if(flag==1)
-    cout<<"Found at : "<<mid+1;
-    else
-    cout<<"not found";
+	int lo = 0, hi = v.size() - 1;
+	int mid;
+	// This below check covers all cases , so need to check
+	// for mid=lo-(hi-lo)/2
+	while (hi - lo > 1) {
+		int mid = (hi + lo) / 2;
+		if (v[mid] < To_Find) {
+			lo = mid + 1;
+		}
+		else {
+			hi = mid;
+		}
+	}
+	if (v[lo] == To_Find) {
+		cout << "Found"
+			<< " At Index " << lo << endl;
+	}
+	else if (v[hi] == To_Find) {
+		cout << "Found"
+			<< " At Index " << hi << endl;
+	}
+	else {
+		cout << "Not Found" << endl;
+	}
 }
+
 int main()
 {
-    int n;
-    cin>>n;
-    int arr[n];
-    for (int  i = 0; i < n; i++)
-    {
-        cin>>arr[i];
-    }
-    int s;
-    cin>>s;
-    bsearch(arr,n,s);
+	vector<int> v = { 1, 3, 4, 5, 6 };
+	int To_Find = 1;
+	binarySearch(v, To_Find);
+	To_Find = 6;
+	binarySearch(v, To_Find);
+	To_Find = 10;
+	binarySearch(v, To_Find);
+	return 0;
 }
